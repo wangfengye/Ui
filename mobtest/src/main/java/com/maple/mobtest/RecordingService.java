21955dfce02ed63d60607040a31536233bee3631
wangfengye <1040441325@qq.com>
2018-11-26 16:10:54 +0800

    mobtest异常处理

---
+++ mobtest/src/main/java/com/maple/mobtest/RecordingService.java
@@ -0,0 +1,111 @@
+package com.maple.mobtest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.TimerTask;
+
+/**
+ * 录音的 Service
+ *
+ * Created by developerHaoz on 2017/8/12.
+ */
+
+public class RecordingService extends Service {
+
+    private static final String LOG_TAG = "RecordingService";
+
+    private String mFileName = null;
+    private String mFilePath = null;
+
+    private MediaRecorder mRecorder = null;
+
+    private long mStartingTimeMillis = 0;
+    private long mElapsedMillis = 0;
+    private TimerTask mIncrementTimerTask = null;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startRecording();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mRecorder != null) {
+            stopRecording();
+        }
+        super.onDestroy();
+    }
+
+    public void startRecording() {
+        setFileNameAndPath();
+
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mRecorder.setOutputFile(mFilePath);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mRecorder.setAudioChannels(1);
+        mRecorder.setAudioSamplingRate(44100);
+        mRecorder.setAudioEncodingBitRate(192000);
+
+        try {
+            mRecorder.prepare();
+            mRecorder.start();
+            mStartingTimeMillis = System.currentTimeMillis();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    public void setFileNameAndPath() {
+        int count = 0;
+        File f;
+
+        do {
+            count++;
+            mFileName = "mob_" + (System.currentTimeMillis()) + ".mp4";
+            mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFilePath += "/SoundRecorder/" + mFileName;
+            f = new File(mFilePath);
+        } while (f.exists() && !f.isDirectory());
+    }
+
+    public void stopRecording() {
+
+        mRecorder.stop();
+        mElapsedMillis = (System.currentTimeMillis() - mStartingTimeMillis);
+        mRecorder.release();
+
+        getSharedPreferences("sp_name_audio", MODE_PRIVATE)
+                .edit()
+                .putString("audio_path", mFilePath)
+                .putLong("elpased", mElapsedMillis)
+                .apply();
+        if (mIncrementTimerTask != null) {
+            mIncrementTimerTask.cancel();
+            mIncrementTimerTask = null;
+        }
+
+        mRecorder = null;
+    }
+
+}
